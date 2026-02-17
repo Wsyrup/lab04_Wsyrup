d411dfa225e921d10ac836f09ab4871effdda5ec
Christian Kantchev <cvk@ucsb.edu>
2026-02-16 21:36:32 -0800

    added name to files, implemented top and empty

--- heap.cpp
+++ heap.cpp
@@ -1,5 +1,5 @@
 // heap.cpp
-// Diba Mirza
+// Christian Kantchev
 
 #include "heap.h"
 #include <iostream>
@@ -8,29 +8,33 @@ using std::cout;
 // Builds a heap from the range [start, end) using the heapify algorithm
 // Should run in O(n) time
 Heap::Heap(std::vector<int>::iterator start, std::vector<int>::iterator end){
-
+  //O(n) average because half of the nodes are never pushed down, a fourth are pushed
+  //down only once, etc...
 }
 
 // Pushes a value into the heap, then ensures
 // the heap is correctly arranged
 void Heap::push(int value){
-
+  //needs to reheapify upward
 }
 
 // Pops the minimum value off the heap
 // (but does not return it), then ensures
 // the heap is correctly arranged
 void Heap::pop(){
-
+  //needs to reheapify downward
 }
 
 // Returns the minimum element in the heap
 int Heap::top(){
-  return 0;
+  return (vdata.size() == 0) ? NULL : vdata[0];
 }
 
 // Returns true if the heap is empty, false otherwise
 bool Heap::empty(){
-  return true;
+  if (vdata.size() == 0) {
+    return true;
+  }
+  return false;
 }
     
--- heap.h
+++ heap.h
@@ -1,6 +1,6 @@
 // heap.h
 // Binary heap tree header file for CS 24 lab
-// Diba Mirza
+// Christian Kantchev
 
 #ifndef HEAP_H
 #define HEAP_H
